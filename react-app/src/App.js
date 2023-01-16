51212fa3d4a304168f7bfeec93b630004d9d7982
E Bolton <eebmagic@gmail.com>
2023-01-16 18:26:36 -0500

    fix fetch call

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       // setWaiting(true);
       const requestURL = `${CONFIG.serverOnHTTPS ? "https" : "http"}://${CONFIG.pyServerAddress}:${CONFIG.pyServerPort}?url=${encodeURIComponent(url)}&email=${encodeURIComponent(userEmail)}&username=${encodeURIComponent(username)}`;
       console.log(`Making request at URL: ${requestURL}}`);
-      fetch(requestURL, {mode: 'no-cors'})
+      fetch(requestURL)
         .then(response => {
           console.log(`Got response:`);
           console.log(response);
